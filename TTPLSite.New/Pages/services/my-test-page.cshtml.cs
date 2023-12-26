086dcea3cfe868940c948876fc58936847f226f3
Bhavesh Thummar <bthummar@epathusa.net>
2023-12-26 22:32:44 +0530

    update some page data

---
+++ TTPLSite.New/Pages/services/my-test-page.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TTPLSite.New.Pages.services
+{
+    public class my_test_pageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
