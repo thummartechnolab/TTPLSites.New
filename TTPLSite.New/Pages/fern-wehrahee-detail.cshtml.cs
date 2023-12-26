698fd0e54156ef4d97fb69ce6ea2c4ce8767dbad
Jitender <jitender.thakur@epathusa.net>
2023-12-26 08:53:32 -0600

    code committed with latestv changes

---
+++ TTPLSite.New/Pages/fern-wehrahee-detail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TTPLSite.New.Pages
+{
+    public class fern_wehrahee_detailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TTPLSite.New/Pages/warehouse-management-system.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TTPLSite.New.Pages
+{
+    public class warehouse_management_systemModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
