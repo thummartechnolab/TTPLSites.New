dceea0edd131130eb266bd4adb9dc54b041333c7
Jitender <jitender.thakur@epathusa.net>
2023-12-26 07:50:30 -0600

    added new changes

---
+++ TTPLSite.New/Pages/Industry/finttech.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TTPLSite.New.Pages.Industry
+{
+    public class finttechModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ TTPLSite.New/Pages/Industry/healthcare.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TTPLSite.New.Pages.Industry
+{
+    public class healthcareModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
