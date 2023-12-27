2e894559a2ddf58c071a6e38fb2ce21f349339c6
Jitender <jitender.thakur@epathusa.net>
2023-12-27 02:51:35 -0600

    code committed for ttpl changes

---
+++ TTPLSite.New/Pages/technologies/hire-flutter-developer-technologies.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TTPLSite.New.Pages.technologies
+{
+    public class hire_flutter_developer_technologiesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
